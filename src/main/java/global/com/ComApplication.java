0056996b7aac91d18d1db8ea33d11ba81200d7d5
guo <994501057@qq.com>
2019-09-20 14:48:19 +0800

    maven+tomcat自动部署

--- src/main/java/global/com/ComApplication.java
+++ src/main/java/global/com/ComApplication.java
@@ -2,15 +2,22 @@ package global.com;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class ComApplication {
+public class ComApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
 
         SpringApplication.run(ComApplication.class, args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(ComApplication.class);
     }
 
 }
